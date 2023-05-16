fd61df4706351cc6cbaa04b66860b723a30621b8
Farid Tabatabaie <farid.t@gmail.com>
2023-05-15 20:11:57 -0700

    Add skeleton code for basic argument parsing

--- human_emotion/interface/console_app.py
+++ human_emotion/interface/console_app.py
@@ -4,7 +4,7 @@ import argparse
 class ConsoleApp:
     def __init__(self, args):
         # Initialize any necessary variables and objects based on command-line arguments
-        print(args)
+        pass
 
     def run(self):
         # Main program logic goes here
@@ -14,6 +14,19 @@ class ConsoleApp:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # Define command-line arguments and options using argparse
+    parser.add_argument(
+        "--face_image_path", type=str, help="path to a file containing an isolated image of a face"
+    )
+    parser.add_argument(
+        "--top_n", default=1, type=int, help="maximum number of emotions to return per face"
+    )
+    parser.add_argument(
+        "--ret",
+        default="text",
+        type=str,
+        choices=["text", "num"],
+        help="whether to return emotion labels as text ('text') or numbers ('num')",
+    )
     args = parser.parse_args()
 
     app = ConsoleApp(args)
