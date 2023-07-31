12f439b6b2040a66963e3628c2b9728426c376e5
Farid Tabatabaie <farid.t@gmail.com>
2023-07-30 17:24:58 -0700

    Remove default values when reading in env vars
    See https://trello.com/c/KDtgQ87e

--- human_emotion/interface/app_config_provider.py
+++ human_emotion/interface/app_config_provider.py
@@ -3,14 +3,10 @@ import os
 
 class AppConfigProvider:
     def __init__(self):
-        self._model_path = os.environ.get("MODEL_PATH", "../models/model.h5")
-        self._config_path = os.environ.get("CONFIG_PATH", "../config.json")
-        self._image_input_dir = os.environ.get(
-            "IMAGE_INPUT_DIR", "../input_images/"
-        )
-        self._json_output_dir = os.environ.get(
-            "JSON_OUTPUT_DIR", "../output_json/"
-        )
+        self._model_path = os.environ.get("MODEL_PATH")
+        self._config_path = os.environ.get("CONFIG_PATH")
+        self._image_input_dir = os.environ.get("IMAGE_INPUT_DIR")
+        self._json_output_dir = os.environ.get("JSON_OUTPUT_DIR")
 
     @property
     def app_config(self):
